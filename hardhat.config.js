c359bbdbe597505a9ad069f756586a247c62173b
Guspring <0xgusfring@gmail.com>
2024-07-25 19:50:17 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["58d228f9808186c452e345a3e50d23e35ca363faf35af3b2090554a8fd497104"], //Your private key starting with "0x" 
+      accounts: ["0x.."], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
